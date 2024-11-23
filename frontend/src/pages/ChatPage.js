65329757d88a45f3b98a172ec505abe2952a235b
AdamYChe <adamyc2018@gmail.com>
2024-11-22 23:32:12 -0500

    Complete initial ChatSideBar implementation

--- frontend/src/pages/ChatPage.js
+++ frontend/src/pages/ChatPage.js
@@ -2,6 +2,7 @@ import axios from "axios";
 import SideBar from "./components/side_bar/SideBar";
 import MessageDisplay from "./components/chat_page/MessageDisplay";
 import MessageBar from "./components/chat_page/MessageBar";
+import ChatSideBar from "./components/chat_page/ChatSideBar";
 import { useState, useEffect } from "react";
 
 const messages = [
@@ -13,9 +14,32 @@ const messages = [
 ];
 
 export const ChatPage = () => {
+    const [conversations, setConversations] = useState([]);
+
+    useEffect(() => {   
+        setConversations([
+            {
+                users: [
+                    { firstName: "John", lastName: "Doe", email: "a@ufl.edu", password: "a" },
+                    { firstName: "Zedong", lastName: "Mao", email: "b@ufl.edu", password: "b" }
+                ],
+                lastMessage: "Testing"
+            },
+            {
+                users: [
+                    { firstName: "John", lastName: "Doe", email: "a@ufl.edu", password: "a" },
+                    { firstName: "Zedong", lastName: "Mao", email: "b@ufl.edu", password: "b" }
+                ],
+                lastMessage: "Testing"
+            }
+        ]);
+    }, []);
+
     return(
         <div className="flex h-screen">
             <SideBar />
+            <ChatSideBar conversations={conversations}/>
+
             <div className="ml-16 w-full overflow-hidden p-6">
                 <MessageDisplay messages={messages} />
                 <MessageBar onSend={() => {console.log('hi')}}/>

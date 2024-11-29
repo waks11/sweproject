f48c477811db56fb73e4db01ad6fed7c71ed56d1
AdamYChe <adamyc2018@gmail.com>
2024-11-29 16:16:40 -0500

    Complete report user and rate user popups frontend. Still need to connect backend.

--- frontend/src/pages/components/chat_page/ActionBar.js
+++ frontend/src/pages/components/chat_page/ActionBar.js
@@ -1,16 +1,16 @@
-const ActionBar = ({ flagUser, archiveChat, isItemPoster }) => {
+const ActionBar = ({ flagUserPopup, archiveChatPopup, isItemPoster }) => {
     return (
         <div className="flex justify-between items-center bg-gray-100 p-4">
             <button 
                 className="text-white rounded-full bg-red-600 p-2.5 hover:bg-red-700 font-medium"
-                onClick={flagUser}
+                onClick={() => flagUserPopup(true)}
             >
                 Report
             </button>
             {isItemPoster && 
                 <button 
                     className="text-white rounded-full bg-blue-600 p-2.5 hover:bg-blue-700 font-medium"
-                    onClick={archiveChat}
+                    onClick={() => archiveChatPopup(true)}
                 >
                     Archive Chat
                 </button>
---
+++ frontend/src/pages/components/rate_user/RateUser.js
@@ -0,0 +1,65 @@
+import { useState, useContext } from 'react';
+import { UserContext } from '../UserContext';
+
+const RateUser = ({ onSubmit, setIsOpen }) => {
+    const [rating, setRating] = useState(0);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        /* onSubmit should set the chat to archived, and utilizing rating, you should be able to update the users' ratings */   
+
+        onSubmit();
+
+        setIsOpen(false);
+    };
+
+    return (
+        <div className="fixed inset-0 bg-black bg-opacity-75 flex justify-center items-center">
+            <div className="bg-slate-900 p-20 rounded-lg shadow-lg">
+                <h1 className="text-xl text-slate-100 font-semibold mb-4">
+                    Rate User
+                </h1>
+                <form onSubmit={handleSubmit}>
+                    <div className="mb-4">
+                        <label className="block mb-1 text-slate-100">
+                            Please rate the other user in this chat.
+                        </label>
+                        {[1, 2, 3, 4, 5].map((value) => (
+                            <span
+                                key={value}
+                                className={`cursor-pointer text-3xl ${
+                                    value <= rating ? 'text-yellow-500' : 'text-gray-400'
+                                }`}
+                                onClick={() => setRating(value)}
+                            >
+                                &#9733;
+                            </span>
+                        ))}
+                        <input type="hidden" name="rating" value={rating} />
+                    </div>
+                    <div className="flex w-full justify-around">
+                        <button 
+                            type="submit"
+                            className={`bg-stone-900 text-slate-100 py-2 px-4 rounded-full border border-slate-100 ${
+                                rating === 0 ? 'bg-gray-800 cursor-not-allowed' : 'hover:bg-blue-600' 
+                            }`}
+                            disabled={rating === 0}
+                        >
+                            Submit & Archive
+                        </button>
+                        <button
+                            type="button"
+                            onClick={() => setIsOpen(false)}
+                            className="bg-stone-900 text-slate-100 py-2 px-6 rounded-full border border-slate-100 hover:bg-blue-600"
+                        >
+                            Cancel
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
+};
+
+export default RateUser;
